7083a38bcb72694f80c74e36a8ca4ab349e94047
apedan <alexandr.pedan@gmail.com>
2014-03-21 10:54:12 +0200

    semantic css

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,7 +25,7 @@ class AppKernel extends Kernel
 
             new Vputi\UserBundle\UserBundle(),
             new Vputi\EventBundle\EventBundle(),
-
+            new Vputi\MainBundle\MainBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Vputi/MainBundle/MainBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Vputi\MainBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MainBundle extends Bundle
+{
+}
